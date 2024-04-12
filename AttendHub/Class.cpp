705c780258ec156468064ed5a7d45ac42f80de06
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2024-04-12 17:24:39 +0530

    removed unnecessary files

--- AttendHub/Class.cpp
+++
@@ -1,4 +0,0 @@
-#include "Class.h"
-
-Class::Class(const std::string& _branch, const std::string& _subject, const std::string& _section, const std::string& _instructorCode)
-    : branch(_branch), subject(_subject), section(_section), instructorCode(_instructorCode) {}
